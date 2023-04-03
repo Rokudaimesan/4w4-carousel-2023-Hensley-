ae2c72b15ec841f06086ff9b801c8bbc4afd98ab
Hensley Pierre <e6133965@cmaisonneuve.qc.ca>
2023-04-03 14:07:57 -0400

    ajout du début de carrousel

--- carrousel.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin name: Carrousel Hensley
- * Author : Hensley Pierre
- * Author URI:
- * Description: Cette extension carrousel permettra d'afficher dans une boîte modale animée les images d'une galerie
- * Version: 1.0.0
- */
-?>
